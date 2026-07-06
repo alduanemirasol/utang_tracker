55732b2bea3a48590516635cf8ebfcd930b24e79
awen <awenmirasol234@gmail.com>
2026-07-06 13:08:38 +0800

    use AppFontSizes.lg for bottom nav labels

--- lib/core/presentation/app_shell.dart
+++ lib/core/presentation/app_shell.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:utang_tracker/core/constants/app_colors.dart';
+import 'package:utang_tracker/core/constants/app_font_sizes.dart';
 
 class AppShell extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
@@ -10,23 +12,34 @@ class AppShell extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: navigationShell,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: navigationShell.currentIndex,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.dashboard),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) => navigationShell.goBranch(index),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: AppColors.textSecondary,
+        showUnselectedLabels: true,
+        selectedFontSize: AppFontSizes.md,
+        unselectedFontSize: AppFontSizes.md,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
             label: 'Dashboard',
           ),
-          NavigationDestination(
-            icon: Icon(Icons.receipt_long),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            activeIcon: Icon(Icons.receipt_long),
             label: 'Debts',
           ),
-          NavigationDestination(
-            icon: Icon(Icons.people),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outline),
+            activeIcon: Icon(Icons.people),
             label: 'Customers',
           ),
         ],
-        onDestinationSelected: (index) => navigationShell.goBranch(index),
       ),
     );
   }
